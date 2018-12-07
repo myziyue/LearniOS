f7e14d0cab892921c9f1ca46f260425307f176b8
BiZhiming <evan2884@gmail.com>
2018-12-07 17:45:42 +0800

    ch02-2.1

---
+++ ch02/SHelloWorld/SHelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SHelloWorld
+//
+//  Created by MyZiyue on 2018/12/7.
+//  Copyright © 2018 MyZiyue.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
