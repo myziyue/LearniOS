aa7268717d3c8a9d9b15d98ae1c3398c92f2a038
BiZhiming <evan2884@gmail.com>
2018-12-11 15:17:25 +0800

    代码

--- ch04/SLabelButton1/SLabelButton1/RootViewController.swift
+++ ch04/SLabelButton1/SLabelButton1/RootViewController.swift
@@ -13,7 +13,37 @@ class RootViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let screen = UIScreen.main.bounds
+        
+        let labelWidth:CGFloat = 90
+        let labelHeight:CGFloat = 20
+        let labelTopView:CGFloat = 150
+        let label = UILabel(frame: CGRect(x:(screen.size.width - labelWidth)/2, y:labelTopView, width:labelWidth, height:labelHeight))
+        
+        label.text = "Label"
+        label.textAlignment = .center
+        self.view.addSubview(label)
+        
+        let button = UIButton(type: UIButton.ButtonType.system)
+        button.setTitle("OK", for: UIControl.State.normal)
+        
+        let buttonWidth:CGFloat = 60
+        let buttonHeight:CGFloat = 20
+        let buttonTopView:CGFloat = 240
+        
+        button.frame = CGRect(x:(screen.size.width - buttonWidth)/2, y:buttonTopView, width:buttonWidth, height:buttonHeight)
+        
+        button.addTarget(self, action: #selector(onClick), for: .touchUpInside)
+        
+        self.view.addSubview(button)
+    }
+    
+    @objc func onClick(sender: AnyObject) {
+        NSLog("OK Button onClick.")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
     
 
