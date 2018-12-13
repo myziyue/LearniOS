6c7907a860b4acf8547b1a5506926041db58f017
BiZhiming <evan2884@gmail.com>
2018-12-13 10:00:10 +0800

    AlertView Sheet

--- ch04/AlertView/AlertView/ViewController.m
+++ ch04/AlertView/AlertView/ViewController.m
@@ -18,6 +18,8 @@
     [super viewDidLoad];
     
     CGRect screen = [[UIScreen mainScreen] bounds];
+    
+    // 1. AlertView
     UIButton* buttonAlertView = [UIButton buttonWithType:UIButtonTypeSystem];
     [buttonAlertView setTitle:@"Test警告框" forState: UIControlStateNormal];
     
@@ -29,6 +31,19 @@
     // 指定事件处理方法
     [buttonAlertView addTarget:self action:@selector(testAlertView:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:buttonAlertView];
+    
+    // 2. AlertViewSheet
+    UIButton* buttonActionSheet = [UIButton buttonWithType:UIButtonTypeSystem];
+    [buttonActionSheet setTitle:@"Test操作表" forState:UIControlStateNormal];
+    
+    CGFloat buttonActionSheetWidth = 100;
+    CGFloat buttonActionSheetHeight = 30;
+    CGFloat buttonActionSheetTopView = 260;
+    
+    buttonActionSheet.frame = CGRectMake((screen.size.width - buttonActionSheetWidth)/2, buttonActionSheetTopView, buttonActionSheetWidth, buttonActionSheetHeight);
+    // 指定事件处理方法
+    [buttonActionSheet addTarget:self action:@selector(testActionSheet:) forControlEvents:UIControlEventTouchUpInside];;
+    [self.view addSubview:buttonActionSheet];
 }
 
 -(void)testAlertView:(id)sender {
@@ -49,5 +64,24 @@
     [self presentViewController:alertController animated:true completion: nil];
 }
 
+-(void)testActionSheet:(id)sender {
+    UIAlertController* alertControllerSheet = [[UIAlertController alloc] init];
+    
+    UIAlertAction* cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        NSLog(@"Tap Cancel Button");
+    }];
+    UIAlertAction* destructionAction = [UIAlertAction actionWithTitle:@"Destruction" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+        NSLog(@"Tap Destruction Button");
+    }];
+    UIAlertAction* otherAction = [UIAlertAction actionWithTitle:@"Others" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        NSLog(@"Tap Others Button");
+    }];
+    
+    [alertControllerSheet addAction:cancelAction];
+    [alertControllerSheet addAction:destructionAction];
+    [alertControllerSheet addAction:otherAction];
+    
+    [self presentViewController:alertControllerSheet animated:true completion:nil];
+}
 
 @end
--- ch04/SAlertView/SAlertView/ViewController.swift
+++ ch04/SAlertView/SAlertView/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         let screen = UIScreen.main.bounds
+        
+        // 1. AlertView
         let buttonAlertView = UIButton(type: UIButton.ButtonType.system)
         buttonAlertView.setTitle("Test警告框", for: UIControl.State.normal)
         
@@ -25,6 +27,19 @@ class ViewController: UIViewController {
         // 指定事件处理方法
         buttonAlertView.addTarget(self, action: #selector(testAlertView), for: UIControl.Event.touchUpInside)
         self.view.addSubview(buttonAlertView)
+        
+        // 2. AlertViewSheet
+        let buttonActionSheet = UIButton(type: UIButton.ButtonType.system)
+        buttonActionSheet.setTitle("Test操作表", for: UIControl.State.normal)
+        
+        let buttonActionSheetWidth: CGFloat = 100
+        let buttonActionSheetHeight: CGFloat = 30
+        let buttonActionSheetTopView: CGFloat = 260
+        
+        buttonActionSheet.frame = CGRect(x:(screen.size.width - buttonActionSheetWidth)/2, y:buttonActionSheetTopView, width:buttonActionSheetWidth, height:buttonActionSheetHeight)
+        // 指定事件处理方法
+        buttonActionSheet.addTarget(self, action: #selector(testActionSheet), for: UIControl.Event.touchUpInside)
+        self.view.addSubview(buttonActionSheet)
     }
     
     @objc func testAlertView(sender: AnyObject) {
@@ -43,6 +58,26 @@ class ViewController: UIViewController {
         
         // 显示
         self.present(alertController, animated: true, completion: nil)
+    }
+    
+    @objc func testActionSheet(sender: AnyObject) {
+        let actionSheetController: UIAlertController = UIAlertController()
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel) { (alertAction) -> Void in
+            NSLog("Tap Cancel Button")
+        }
+        let destructiveAction = UIAlertAction(title: "破坏性按钮", style: UIAlertAction.Style.destructive) { (alertAction) -> Void in
+            NSLog("Tap Destructive Button")
+        }
+        let otherAction = UIAlertAction(title: "其他按钮", style: UIAlertAction.Style.default) { (alertAction) -> Void in
+            NSLog("Tap Others Button")
+        }
+        
+        actionSheetController.addAction(cancelAction)
+        actionSheetController.addAction(destructiveAction)
+        actionSheetController.addAction(otherAction)
+        
+        self.present(actionSheetController, animated: true, completion: nil)
     }
 
 
