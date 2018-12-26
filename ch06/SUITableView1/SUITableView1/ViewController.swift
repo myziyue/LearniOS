98e296802c51b810109303d77f39c16e52cb7a7b
BiZhiming <evan2884@gmail.com>
2018-12-26 18:20:16 +0800

    UITableView

--- ch06/SUITableView1/SUITableView1/ViewController.swift
+++ ch06/SUITableView1/SUITableView1/ViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let CellIdentifier = "CellIndentifier"
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    var listTeams: NSArray!
+    var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let plistPath = Bundle.main.path(forResource: "team", ofType: "plist")
+        // 获取属性列表文件中的全部数据
+        self.listTeams = NSArray(contentsOfFile: plistPath!)
+        
+        self.tableView = UITableView(frame: self.view.frame, style: .plain)
+        
+        // 设置表视图委托对象为self
+        self.tableView.delegate = self
+        // 设置表视图数据对象为self
+        self.tableView.dataSource = self
+        
+        self.view.addSubview(self.tableView)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.listTeams.count
     }
 
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: CellIdentifier)
+        if(cell == nil) {
+            cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: CellIdentifier)
+        }
+        
+        let row = indexPath.row
+        
+        let rowDict = self.listTeams[row] as! NSDictionary
+        cell.textLabel?.text = rowDict["name"] as? String
+        
+        let imagePath = String(format: "%@.png", rowDict["image"] as! String)
+        cell.imageView?.image = UIImage(named: imagePath)
+        
+        cell.accessoryType = .disclosureIndicator
+        
+        return cell
+    }
 }
 
--- ch06/UITableView1/UITableView1/ViewController.m
+++ ch06/UITableView1/UITableView1/ViewController.m
@@ -8,7 +8,12 @@
 
 #import "ViewController.h"
 
+#define CellIdentifier @"CellIdentifier"
+
 @interface ViewController ()
+
+@property (nonatomic, strong) NSArray* listTeams;
+@property (nonatomic, strong) UITableView* tableView;
 
 @end
 
@@ -16,8 +21,42 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSBundle* bundle = [NSBundle mainBundle];
+    NSString* plistPath = [bundle pathForResource:@"team" ofType:@"plist"];
+    
+    self.listTeams = [[NSArray alloc] initWithContentsOfFile: plistPath];
+    
+    self.tableView = [[UITableView alloc] initWithFrame:self.view.frame style:UITableViewStylePlain];
+    
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    
+    [self.view addSubview:self.tableView];
 }
 
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.listTeams count];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    if(cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    NSUInteger row = [indexPath row];
+    
+    NSDictionary* rowDict = self.listTeams[row];
+    cell.textLabel.text = rowDict[@"name"];
+    
+    NSString* imagePath = [[NSString alloc] initWithFormat:@"%@.png", rowDict[@"image"]];
+    cell.imageView.image = [UIImage imageNamed:imagePath];
+    
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    
+    return cell;
+}
 
 @end
